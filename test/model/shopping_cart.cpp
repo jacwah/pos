35ae0afd2017b12844f1b9e91267db2f8635aa93
Jacob Wahlgren <jacob@dstsrc.net>
2018-05-06 13:04:45 +0200

    Test ShoppingCartSummary

--- test/model/shopping_cart.cpp
+++ test/model/shopping_cart.cpp
@@ -6,9 +6,11 @@ TEST_CASE("Shopping cart works with items")
     integration::ItemCatalog itemCatalog;
     integration::ItemId itemId(1);
     integration::ItemId itemId2(2);
+    integration::ItemId invalidId(0);
     integration::Item item = *itemCatalog.find(itemId);
     integration::Item item2 = *itemCatalog.find(itemId2);
     model::ShoppingCart cart(itemCatalog);
+    model::ShoppingCartSummary summary({}, {});
 
     SECTION("ItemRecords can increment quantity")
     {
@@ -37,11 +39,23 @@ TEST_CASE("Shopping cart works with items")
         REQUIRE(++iter == cart.end());
     }
 
-    SECTION("Gross price is correct")
+    SECTION("Gross price and summary is correct")
     {
         util::Amount correctPrice = item.getPrice() * 3 + item2.getPrice() * 4;
-        cart.addIfValid(itemId, 3);
-        cart.addIfValid(itemId2, 4);
-        REQUIRE(cart.calculateGrossPrice() == Approx(correctPrice));
+        summary = cart.addIfValid(itemId, 3);
+        CHECK(summary.getLastAddedItem()->getDescription() == item.getDescription());
+        CHECK(summary.getLastAddedItem()->getPrice() == item.getPrice());
+        summary = cart.addIfValid(itemId2, 4);
+        CHECK(summary.getLastAddedItem()->getDescription() == item2.getDescription());
+        CHECK(summary.getLastAddedItem()->getPrice() == item2.getPrice());
+        CHECK(cart.calculateGrossPrice() == Approx(correctPrice));
+        CHECK(summary.getGrossPrice() == Approx(correctPrice));
+    }
+
+    SECTION("Invalid id is indicated with no last added")
+    {
+        summary = cart.addIfValid(invalidId, 1);
+        CHECK_FALSE(summary.getLastAddedItem());
+        CHECK(summary.getGrossPrice() == util::Amount(0));
     }
 }
