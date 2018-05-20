401af24170ec9215a55ab6550c881dcd948d9a50
Jacob Wahlgren <jacob@dstsrc.net>
2018-05-20 20:28:26 +0200

    Add doc comments to Logger

--- src/util/logger.h
+++ src/util/logger.h
@@ -4,12 +4,26 @@
 
 namespace util {
 
+/**
+ * Logs messages to a file aimed at sysadmins and developers.
+ */
 class Logger {
     std::fstream logFile;
     Logger(const char *filename);
 
 public:
+    /**
+     * Gets the single instance.
+     *
+     * @return The single instance.
+     */
     static Logger& getInstance();
+
+    /**
+     * Logs an exception to the file.
+     *
+     * @param exception The exception.
+     */
     void logException(const std::exception& exception);
 };
 
